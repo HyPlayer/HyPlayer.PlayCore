c2f92519ed5670f592c9ee95ad855f3030b638a5
kengwang <atkengwang@qq.com>
2023-08-02 12:34:51 +0800

    [refact] Modify ProvidableTypes

--- HyPlayer.PlayCore.Abstraction/ProviderBase.cs
+++ HyPlayer.PlayCore.Abstraction/ProviderBase.cs
@@ -4,5 +4,7 @@ public abstract class ProviderBase
 {
     public abstract string Name { get; }
     public abstract string Id { get; }
-    public abstract Dictionary<string, string> TypeIdToNameDictionary { get; }
+    public abstract List<ProvidableTypeId> ProvidableTypeIds { get; }
 }
+
+public record ProvidableTypeId(string Id, string Name, bool Observable);
