2fd222f764a3b586be9a875da6aabccad4e329b3
kengwang <atkengwang@qq.com>
2023-08-03 23:00:52 +0800

    [rev] Remove IStoredItemsProvidable
    Please Move to LikedItems

--- HyPlayer.PlayCore.Abstraction/Interfaces/Provider/IStoredItemsProvidable.cs
+++
@@ -1,8 +0,0 @@
-﻿using HyPlayer.PlayCore.Abstraction.Models;
-
-namespace HyPlayer.PlayCore.Abstraction.Interfaces.Provider;
-
-public interface IStoredItemsProvidable
-{
-    public Task<ContainerBase?> GetStoredItems(string typeId);
-}
