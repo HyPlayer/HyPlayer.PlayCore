367f888223660759f5f0f79a427ebfd6bbaabc27
kengwang <atkengwang@qq.com>
2023-08-28 19:58:54 +0800

    [refact] Change ItemId Name

--- HyPlayer.PlayCore.Abstraction/Models/ProvidableItemBase.cs
+++ HyPlayer.PlayCore.Abstraction/Models/ProvidableItemBase.cs
@@ -3,7 +3,7 @@
 public abstract class ProvidableItemBase
 {
     public required string Name { get; set; }
-    public string Id => $"{ProviderId}{TypeId}{ActualId}";
+    public string ItemId => $"{ProviderId}{TypeId}{ActualId}";
     public abstract string ProviderId { get; }
     public abstract string TypeId { get; }
     public required string ActualId { get; set; }
