2c74a6deab61eb766399ea65dc02a5df83fef2f9
zhh134 <zhh13950510580@outlook.com>
2026-02-25 14:00:27 +0800

    Feat: 添加启动加载PlayCore.

--- HyPlayer.PlayCore.DemoApp/App.xaml.cs
+++ HyPlayer.PlayCore.DemoApp/App.xaml.cs
@@ -15,9 +15,14 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Depository.Core;
+using Depository.Extensions;
+using Depository.Abstraction.Interfaces;
+using HyPlayer.PlayCore.Abstraction;
+using HyPlayer.PlayCore.Implementation.AudioGraphService;
+using HyPlayer.PlayCore.PlayListControllers;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+
 
 namespace HyPlayer.PlayCore.DemoApp
 {
@@ -28,6 +33,21 @@ namespace HyPlayer.PlayCore.DemoApp
     {
         private Window? _window;
 
+        public static IDepository Services { get; private set; }
+
+        public static object GetService<TService>()
+            where TService : class
+        {
+            try
+            {
+                return Services.Resolve<TService>();
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -35,15 +55,28 @@ namespace HyPlayer.PlayCore.DemoApp
         public App()
         {
             InitializeComponent();
+
+            var container = DepositoryFactory.CreateNew();
+            container.AddSingleton<PlayCoreBase, Chopin>();
+            container.AddSingleton<MainWindow>();
+            Services = container;
         }
 
         /// <summary>
         /// Invoked when the application is launched.
         /// </summary>
         /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        protected override async void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            _window = new MainWindow();
+            var playCore = (Chopin) GetService<PlayCoreBase>();
+            if (playCore != null)
+            {
+                await playCore.RegisterAudioServiceAsync(typeof(AudioGraphService));
+                await playCore.RegisterPlayListControllerAsync(typeof(DefaultPlayListManager));
+                await playCore.RegisterPlayListControllerAsync(typeof(OrderedRollPlayController));
+            }
+
+            _window = (Window) GetService<MainWindow>();
             _window.Activate();
         }
     }
